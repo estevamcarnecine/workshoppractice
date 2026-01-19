a9407e5daa885311c277621022ba4fd0b299c86e
Estevam <estvmc@gmail.com>
2026-01-19 07:18:28 -0300

    project created

---
+++ src/main/java/com/estevamcarnecine/workshoppractice/WorkshoppracticeApplication.java
@@ -0,0 +1,13 @@
+package com.estevamcarnecine.workshoppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoppracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoppracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estevamcarnecine/workshoppractice/WorkshoppracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estevamcarnecine.workshoppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoppracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
