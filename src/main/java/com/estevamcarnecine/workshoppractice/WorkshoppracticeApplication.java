f862d0ef3f4d90bdb7b91e38f3bcae7304f61aaa
Estevam Carnecine Silva <estvmc@gmail.com>
2026-02-27 11:17:58 -0300

    Conectando ao MongoDB com Repository e Service

--- src/main/java/com/estevamcarnecine/workshoppractice/WorkshoppracticeApplication.java
+++ src/main/java/com/estevamcarnecine/workshoppractice/WorkshoppracticeApplication.java
@@ -1,7 +1,12 @@
 package com.estevamcarnecine.workshoppractice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.estevamcarnecine.workshoppractice.domain.User;
+import com.estevamcarnecine.workshoppractice.repository.UserRepository;
 
 @SpringBootApplication
 public class WorkshoppracticeApplication {
@@ -10,4 +15,13 @@ public class WorkshoppracticeApplication {
 		SpringApplication.run(WorkshoppracticeApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserRepository repo) {
+		return args -> {
+			repo.deleteAll(); 
+			repo.save(new User(null, "alex green", "alex@gmail.com"));
+			repo.save(new User(null, "maria", "maria@gmail.com"));
+			System.out.println("Database seeded!");
+		};
+	}
 }
---
+++ src/main/java/com/estevamcarnecine/workshoppractice/services/UserService.java
@@ -0,0 +1,18 @@
+package com.estevamcarnecine.workshoppractice.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.estevamcarnecine.workshoppractice.domain.User;
+import com.estevamcarnecine.workshoppractice.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+    
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
