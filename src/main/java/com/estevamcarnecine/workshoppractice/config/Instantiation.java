dad497362ffb1696e970b2fc83d074495e6074b7
Estevam Carnecine Silva <estvmc@gmail.com>
2026-03-02 19:21:42 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/estevamcarnecine/workshoppractice/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.estevamcarnecine.workshoppractice.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.estevamcarnecine.workshoppractice.domain.User;
+import com.estevamcarnecine.workshoppractice.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+	userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	
+	}
+
+}
