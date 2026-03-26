f6243a03d12710a50ffff3b3e9d10f4bbd763582
Estevam Carnecine Silva <estvmc@gmail.com>
2026-03-25 23:13:57 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/estevamcarnecine/workshoppractice/resources/UserResource.java
+++ src/main/java/com/estevamcarnecine/workshoppractice/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod; // Importante para
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.estevamcarnecine.workshoppractice.domain.Post;
 import com.estevamcarnecine.workshoppractice.domain.User;
 import com.estevamcarnecine.workshoppractice.dto.UserDTO;
 import com.estevamcarnecine.workshoppractice.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts();
 	}
 }
