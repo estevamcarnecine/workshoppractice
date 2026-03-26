13af3e7170303cac0f72436a738544cd7ffabf1e
Estevam Carnecine Silva <estvmc@gmail.com>
2026-03-25 23:06:36 -0300

    Referenciando os posts do usuário

--- src/main/java/com/estevamcarnecine/workshoppractice/config/Instantiation.java
+++ src/main/java/com/estevamcarnecine/workshoppractice/config/Instantiation.java
@@ -10,18 +10,19 @@ import org.springframework.context.annotation.Configuration;
 
 import com.estevamcarnecine.workshoppractice.domain.Post;
 import com.estevamcarnecine.workshoppractice.domain.User;
+import com.estevamcarnecine.workshoppractice.dto.AuthorDTO;
 import com.estevamcarnecine.workshoppractice.repository.PostRepository;
 import com.estevamcarnecine.workshoppractice.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -35,11 +36,14 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
 	userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	postRepository.saveAll(Arrays.asList(post1, post2));
+	
+	maria.getPosts().addAll(Arrays.asList(post1, post2));
+	userRepository.save(maria);
 	
 	}
 
